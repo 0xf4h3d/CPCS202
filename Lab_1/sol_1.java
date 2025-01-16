c5148836712429b1387a10c2bc5613b2d4a4043a
0xf4h3d <ifo0ody9@gmail.com>
2025-01-16 18:52:56 +0300

    uploading lab 1 files

---
+++ Lab_1/sol_1.java
@@ -0,0 +1,11 @@
+public class sol_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java"); //solution A 
+        //solution B 
+        System.out.println("Welcome");
+        System.out.println("to");
+        System.out.println("Java");
+    }
+    
+}
---
+++ Lab_1/sol_4.java
@@ -0,0 +1,7 @@
+public class sol_4 {
+    public static void main(String[] args) {
+        System.out.println((9.5*4.5-2.5*3)/(45.5-3.5));
+        
+    }
+    
+}
